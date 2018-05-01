02c3553d30476e375e43caee4ecc038c12e436d2
koobohyeon <goosee115@gmail.com>
2018-05-01 20:44:45 +0900

    modified selected array save process"

--- ideal_game_logic.js
+++ ideal_game_logic.js
@@ -1,48 +1,120 @@
-// select 화면에서 선택하면 사진이 선택이 선택되고 어딘가로 저장된다. 
+// 배열을 섞는 함수
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-// 만약 p1을 클릭하면 p1을 배열에 저장한다. 
-// 만약 p2를 클릭하면 p2를 배열에 저장한다. 
-// 다음 두 개의 사진을 보여준다. 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-const arr = ['/img/p0.jpeg','/img/p1.jpeg','/img/p2.jpeg','/img/p3.jpeg','/img/p4.jpeg','/img/p5.jpeg',
-'/img/p6.jpeg','/img/p7.jpeg']
-let index = arr.length-1
-const p1El = document.querySelector('#p1')
-const p2El = document.querySelector('#p2')
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
+  return array;
+}
+
+//두 배열을 바꿔주는 함수
+function swap(arr1, arr2) {
+  let temp = arr1;
+  arr1 = arr2.slice();
+  arr2 = temp.slice();
+  return [arr1, arr2];
+}
+
+let arr = [
+  "/img/p0.jpeg",
+  "/img/p1.jpeg",
+  "/img/p2.jpeg",
+  "/img/p3.jpeg",
+  "/img/p4.jpeg",
+  "/img/p5.jpeg",
+  "/img/p6.jpeg",
+  "/img/p7.jpeg",
+  "/img/p8.jpeg",
+  "/img/p9.jpeg",
+  "/img/p10.jpeg",
+  "/img/p11.jpeg",
+  "/img/p12.jpeg",
+  "/img/p13.jpeg",
+  "/img/p14.jpeg",
+  "/img/photo15.jpeg"
+];
+let selectedArr = [];
+
+arr = shuffle(arr);
+//
+const p1El = document.querySelector("#p1");
+const p2El = document.querySelector("#p2");
+const roundEl = document.querySelector(".round");
+//라운드 설정
+let round = arr.length / 2;
+let current = 1;
 let i = 0;
- p1El.addEventListener('click', () => { 
+roundEl.textContent = `${current} / ${round}`;
+document.getElementById("p1").src = arr[0];
+document.getElementById("p2").src = arr[1];
 
- arr.splice(i,2,arr[i])
- document.getElementById("p1").src = arr[i+1];
- document.getElementById("p2").src = arr[i+2];
-  i++
-  if(arr[i]=== undefined){
+// 첫 번째 사진이 선택됐을 때
+
+p1El.addEventListener("click", () => {
+  selectedArr.push(arr[i]);
+  i += 2;
+  document.getElementById("p1").src = arr[i];
+  document.getElementById("p2").src = arr[i+1];
+  current++;
+  roundEl.textContent = `${current} / ${round}`;
+  // 끝 두 개의 사진만 남았을 때 클릭한다면 클릭된 사진을 저장하고 라운드를 바꿔줘야 한다.
+  if (i === arr.length) {
+    // selectedArr.push(arr[i]);
+    [arr, selectedArr] = swap(arr, selectedArr);
+    selectedArr = [];
     i = 0;
-    document.getElementById('p1').src = arr[i];
-    document.getElementById('p2').src = arr[i+1];
+    current = 1;
+    round = round / 2;
+    if(arr.length !== 1){
+      roundEl.textContent = `${current} / ${round}`;
+    }
+    document.getElementById("p1").src = arr[i];
+    document.getElementById("p2").src = arr[i + 1];
   }
-  if(arr.length === 1){
-    localStorage.setItem('result', arr[0])
-    window.location.href = '/result.html'
+  // 최종선택 
+  if (arr.length === 1) {
+    localStorage.setItem("result", arr);
+    window.location.href = "/result.html";
   }
-}) 
-  p2El.addEventListener('click', () => {
-  arr.splice(i,2,arr[i+1])
-  document.getElementById("p1").src = arr[i+1];
-  document.getElementById("p2").src = arr[i+2];
-  i++
-  if(arr[i+1]=== undefined){
+ 
+});
+//두 번째 사진이 선택됐을 때
+p2El.addEventListener("click", () => {
+  selectedArr.push(arr[i+1]);
+  i += 2;
+  document.getElementById("p1").src = arr[i];
+  document.getElementById("p2").src = arr[i+1];
+  current++;
+
+
+  // 끝 두 개의 사진만 남았을 때 클릭한다면 클릭된 사진을 저장하고 라운드를 바꿔줘야 한다.
+  if (i === arr.length) {
+    // selectedArr.push(arr[i]);
+    [arr, selectedArr] = swap(arr, selectedArr);
+    selectedArr = [];
     i = 0;
-    document.getElementById('p1').src = arr[i];
-    document.getElementById('p2').src = arr[i+1];
+    current = 1;
+    round = round / 2;
+    roundEl.textContent = `${current} / ${round}`;
+    document.getElementById("p1").src = arr[i];
+    document.getElementById("p2").src = arr[i + 1];
   }
-  if(arr.length === 1){
-    localStorage.setItem('result', arr[0])
-    window.location.href = '/result.html'
+  // 최종선택 
+  if (arr.length === 1) {
+    localStorage.setItem("result", arr);
+    window.location.href = "/result.html";
   }
-})
-
-
-
-
+  roundEl.textContent = `${current} / ${round}`;
+});
